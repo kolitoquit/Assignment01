a6fd50b93096ab7e920155283353bbec528d5e15
Krizia Olitoquit <kriziaolitoquit@gmail.com>
2026-06-16 19:51:37 -0700

    final answers for assignment01

--- js/script.js
+++ js/script.js
@@ -1,10 +1,220 @@
 // STEP 1
 
+/*
 let someMonth;;
 function theMonth();
 let currentMonth;
 let summerMonth;
 let MyLibraryFunction;
+*/
 
 // STEP 2
+
+/* 
+2. Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression.
+18              numerical literal expression
+"Assignment 01" string literal expression
+true            Boolean literal expression
+null            null literal expression
+
+*/
+
 // STEP 3
+
+/*
+let isTotal = 10 + 20;
+let isCelsuis = (fahrenheit - 32) * 5 / 9;
+*/
+
+// STEP 4
+
+/*
+
+let firstName           strFirstName
+let lastName            strLastName
+let address             strAddress
+let city                strCity
+let state               strState
+let zipCode             strZipCode
+let yourAge             intYourAge
+let referralSource      strReferralSource
+let mayWeContactYou     blnMayWeContactYou
+
+*/
+
+// STEP 5
+
+/*
+
+let firstName = 'Krizia';
+let lastName = 'Olitoquit';
+let zipCode = '92131';
+
+*/
+
+// STEP 6
+
+/*
+
+let phrase = 'Krizia live in '
+let zipCode = 92131
+let result = phrase + zipCode
+
+console.log(result);
+
+document.getElementById('output').innerText = result;
+
+*/
+
+// STEP 7
+
+/*
+
+// First Variable - Boolean & String
+let firstVariable = "Is the food delicious?"
+let firstBoolean = true
+let firstResult = firstVariable + " " + firstBoolean
+
+console.log(firstResult);
+
+document.getElementById('output1').innerText = firstResult;
+
+// Second Variable - Boolean & String
+let secondVariable = 20
+let secondBoolean = false
+let secondResult = secondVariable + " " + secondBoolean
+
+console.log(secondResult);
+
+document.getElementById('output2').innerText = secondResult;
+*/
+
+//STEP 8
+
+/*
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+
+document.getElementById('output2').innerText = someString;
+*/
+
+// STEP 9
+
+/*
+
+let nullValue = null;
+let undefinedValue;
+
+console.log(nullValue);
+console.log(undefinedValue);
+
+document.getElementById('output1').innerText = `nullValue: ${nullValue}`;
+document.getElementById('output2').innerText = `undefinedValue: ${undefinedValue}`;
+*/
+
+// STEP 10
+/*
+
+let x = 6;
+x++;
+
+alert(x);
+*/
+
+// STEP 11
+/*
+
+let fullName = "Zak Ruvalcaba"
+let phrase = "Hello"
+let classInfo = "welcome to the Javascript class!"
+let result = phrase + " " + fullName + "," + " " + classInfo
+
+alert(result);
+
+
+let fullName = "Krizia Olitoquit";
+let phrase = "Hello";
+let classInfo = "welcome to the Javascript class!";
+let result = phrase + " " + fullName + "," + " " + classInfo;
+
+alert(result);
+*/
+
+// STEP 12
+
+/*
+
+let name = "Krizia";
+alert(name);
+*/
+
+// STEP 13
+
+/*
+
+let course = "Javascript";
+alert(course);
+*/
+
+// STEP 14
+
+/*
+
+let fullName = "Zak Ruvalcaba,\n"
+let phrase = "Hello"
+let classInfo = "Welcome to the Javascript class!"
+let result = phrase + " " + fullName + classInfo
+
+alert(result);
+*/
+
+// STEP 15 & 16
+
+/*
+
+let name = prompt("Enter your name:")
+let course = prompt("Enter your course:")
+
+*/
+
+// STEP 17
+/*
+
+let x = 10
+let y = 20
+let sum = x + y;
+alert(sum);
+
+*/ 
+
+// STEP 18
+
+/*
+
+let x = 20
+let y = 20
+let sum = x + y
+alert(sum);
+
+*/
+
+// STEP 19
+
+/*
+
+let x = 20
+let y = 5
+let product = x * y
+alert(product);
+
+*/
+
+// STEP 20
+
+/*
+
+let x = 20 % 3
+let result = x / 1
+alert(result);
+
+*/
